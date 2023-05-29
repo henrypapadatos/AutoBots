d121f9e8eb6f1c1d44460da30837338b2312d272
JulDib <48629679+juldib@users.noreply.github.com>
2023-05-29 14:11:22 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,1 +1,10 @@
-/datasets/argoverse/dataset.py
+#Path of the dataset.py we use (it is the one provided by Autobots) : /datasets/argoverse/dataset.py
+
+#Here is another representation on the path :
+datasets
+ └──argoverse
+      └──dataset.py
+ 
+#You can also read it directly by clicking here.
+ 
+
