cc4fabb663290080d98526401d7cd4e69e27c0bd
JulDib <48629679+juldib@users.noreply.github.com>
2023-05-29 14:13:33 +0200

    Update dataset.py
    Url added

--- dataset.py
+++ dataset.py
@@ -1,10 +1,11 @@
 #Path of the dataset.py we use (it is the one provided by Autobots) : /datasets/argoverse/dataset.py
 
 #Here is another representation on the path :
+
 datasets
  └──argoverse
       └──dataset.py
  
-#You can also read it directly by clicking here.
+#You can also read it directly by copy/pasting the following url in a new browser tab : https://github.com/henrypapadatos/AutoBots/blob/d121f9e8eb6f1c1d44460da30837338b2312d272/datasets/argoverse/dataset.py
  
 
